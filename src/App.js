e9c00d28a6a6ac812f6b364fdd2e39d380bd3148
Iswearthisisntme <jhkimuniversity@gmail.com>
2024-07-28 22:22:45 -0400

    Prepare for deployment

--- src/App.js
+++ src/App.js
@@ -1,24 +1,192 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import headImage from './images/head.jpg';
+import dreamlogImage from './images/pic01.jpg';
+import { FaJava, FaPython, FaJs, FaReact, FaUnity, FaLinkedin, FaGithub } from 'react-icons/fa';
+import { SiCplusplus, SiGodotengine } from 'react-icons/si';
+import { SiBlender } from "react-icons/si";
+import { SiAdobeillustrator } from "react-icons/si";
+import { SiAdobephotoshop } from "react-icons/si";
+import { PiYarn } from 'react-icons/pi';
+import { IoMdHome } from "react-icons/io";
+import { RiContactsBook2Fill } from "react-icons/ri";
+import { GoProject } from "react-icons/go";
+import { SiHyperskill } from "react-icons/si";
+import { MdEmail } from "react-icons/md";
 
 function App() {
+  const importantSkills = [
+    { name: 'Java', icon: <FaJava /> },
+    { name: 'Python', icon: <FaPython /> },
+    { name: 'C++', icon: <SiCplusplus /> },
+    { name: 'Javascript', icon: <FaJs /> },
+    { name: 'React', icon: <FaReact /> },
+  ];
+
+  const skills = [
+    { name: 'Java', icon: <FaJava /> },
+    { name: 'Python', icon: <FaPython /> },
+    { name: 'C++', icon: <SiCplusplus /> },
+    { name: 'Javascript', icon: <FaJs /> },
+    { name: 'React', icon: <FaReact /> },
+    { name: 'Godot', icon: <SiGodotengine /> },
+    { name: 'Unity', icon: <FaUnity /> },
+    { name: 'Blender', icon: <SiBlender /> },
+    { name: 'Yarn', icon: <PiYarn /> },
+    { name: 'Illustrator', icon: <SiAdobeillustrator/>},
+    { name: 'Photoshop', icon: <SiAdobephotoshop/>}
+  ];
+  const [searchTerm, setSearchTerm] = useState('');
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredSkills = skills.filter(skill =>
+                                           skill.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="page">
+        <div className={`sidemenu ${isOpen ? 'open' : ''}`}>
+          <ul>
+            <li><a href="#header"><IoMdHome /></a>
+              <span className={"tooltip"}>About</span></li>
+            <li><a href="#skills"><SiHyperskill /></a>
+              <span className={"tooltip"}>Skills</span></li>
+            <li><a href="#projects"><GoProject /></a>
+              <span className={"tooltip"}>Projects</span></li>
+            <li><a href="#contacts"><RiContactsBook2Fill /></a>
+              <span className={"tooltip"}>Contacts</span></li>
+          </ul>
+          <div className="arrow">
+            <span>{isOpen ? ' < ' : ' > '}</span>
+          </div>
+        </div>
+
+
+        <div className="header" id="header">
+          <h1>This is Chris Kim!</h1>
+          <img src={headImage} alt="Chris Kim" className="head-image" />
+          <h2 id="skills">A passionate Programmer capable of using but not limited to:</h2>
+          <div className="important-skills" >
+            {importantSkills.map((skill, index) => (
+                <div key={index} className="important-skill-item">
+                  {skill.icon}
+                  <span>{skill.name}</span>
+                </div>
+            ))}
+          </div>
+          <a href="/resume.pdf" download className="download-button">Download Resume</a>
+          <input id="skills"
+                 type="text"
+                 placeholder="Search for other skills Chris knows!"
+                 value={searchTerm}
+                 onChange={handleSearchChange}
+                 className="search-bar"
+          />
+          <div className="skills-list">
+            <ul >
+              {filteredSkills.map((skill, index) => (
+                  <li key={index} className="skill-item">
+                    {skill.icon} {skill.name}
+                  </li>
+              ))}
+            </ul>
+          </div>
+
+
+        </div>
+        <div className="triangle1"></div>
+        <div className="triangle2"></div>
+        <div className="body">
+          {/*<div className="info">*/}
+          {/*    <div className="info-details">*/}
+          {/*        <p><strong>Name:</strong> Juhyun (Chris) Kim</p>*/}
+          {/*        <p><strong>School:</strong> Northeastern University</p>*/}
+          {/*        <p><strong>Language:</strong> Korean, English, Japanese</p>*/}
+          {/*        <p><strong>Major:</strong> Computer Science and Game Dev</p>*/}
+          {/*        <p><strong>Honors:</strong> GPA: 3.6/4.00 | Distinguished Scholar</p>*/}
+          {/*    </div>*/}
+          {/*</div>*/}
+          <div className="projects" id="projects">
+            <div className={"projectleft"}>
+              <img src={dreamlogImage}/>
+              <div className={"projectInfo"}>
+                <h2>Web dev Project: Dream Log</h2>
+                <p>
+                  A Journaling website made with Next.js and firebase that allows users to
+                  write journals and reflect on them through statistics
+                </p>
+                <a href={""} >
+                  <div className={"projectLink"}>
+                    Live App
+                  </div>
+                </a>
+              </div>
+            </div>
+
+            <div className={"projectright"}>
+              <div className={"projectInfo"}>
+                <h2>Game Dev Project: 1v1 Fighting Game</h2>
+                <p>
+                  A 1 vs 1 fighting game made in Javascript that can be played
+                  through a browser. Has multiple characters, moveset and more.
+                </p>
+                <a href={""} >
+                  <div className={"projectLink"}>
+                    Live App
+                  </div>
+                </a>
+              </div>
+              <img src={dreamlogImage}/>
+
+            </div>
+
+
+          </div>
+        </div>
+
+        <h1>Contact Chris!</h1>
+        <div className="contact" id="contacts">
+          <div className={"links"}>
+            <p>
+              <a href="mailto:jhkimuniversity@gmail.com"><MdEmail/> jhkimuniversity@gmail.com</a>
+              <a href="https://www.linkedin.com/in/your-profile" target="_blank" rel="noopener noreferrer"><FaLinkedin /> LinkedIn</a>
+              <a href="https://github.com/your-profile" target="_blank" rel="noopener noreferrer"><FaGithub /> GitHub</a>
+            </p>
+          </div>
+          <form name="msgme">
+            <input
+                type="text"
+                name="name"
+                placeholder="Your Name"
+                className="contact-input"
+                required
+            />
+            <input
+                type="text"
+                name="email"
+                placeholder="Your Email"
+                className="contact-input"
+                required
+            />
+            <textarea
+                name="message"
+                row = "6"
+                placeholder="Your Message"
+                className="contact-input"
+                required
+            ></textarea>
+            <button type="submit">Send Message</button>
+          </form>
+        </div>
+        <div className={"background"}>helo</div>
+      </div>
   );
 }
 
